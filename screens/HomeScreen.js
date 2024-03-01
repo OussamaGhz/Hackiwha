072bcac1288e29957dc9da64b4e5e2340294e25f
SohaibGhz <sohaib.ghazi@outlook.fr>
2024-03-01 16:15:01 +0100

    finfishing the home page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text, Button, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
 
 // HomeScreen.js
 
@@ -7,15 +8,37 @@ const HomeScreen = ({ navigation }) => {
   return (
     <>
       <View style={styles.container}>
-        <View style={styles.imageContainer}></View>
-        <Image source={require("../assets/image.png")} style={styles.image} />
-      </View>
-      <Text style={styles.boldText}>Bold Text</Text>
-      <Text style={styles.descriptionText}>Description Text</Text>
-      <View style={styles.buttonContainer}>
-        <Button title="Login" onPress={() => navigation.navigate("Login")} />
+        <View style={styles.imageContainer}>
+          <Image source={require("../assets/image.png")} style={styles.image} />
+        </View>
 
-        <Button title="Signup" onPress={() => navigation.navigate("Signup")} />
+        <View style={styles.textContainer}>
+          <Text style={styles.boldText}>
+            Accusamus est porro rem odio sed doloremque voluptate mollitia.
+          </Text>
+          <Text style={styles.descriptionText}>
+            Ipsam quibusdam deserunt illo voluptatem. Non et ipsam ducimus et.
+            Adipisci et mollitia nesciunt autem repudiandae est et. Sit iusto
+            aut vel laboriosam dolores fugit sed et eum. Pariatur praesentium
+            omnis. Sint ut autem cumque.
+          </Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button mode="contained" onPress={() => navigation.navigate("Login")}>
+            Login
+          </Button>
+
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View style={{ flex: 1, height: 1, backgroundColor: "black" }} />
+            <View>
+              <Text style={{ width: 50, textAlign: "center" }}>or</Text>
+            </View>
+            <View style={{ flex: 1, height: 1, backgroundColor: "black" }} />
+          </View>
+          <Button mode="outlined" onPress={() => navigation.navigate("Signup")}>
+            Signup
+          </Button>
+        </View>
       </View>
     </>
   );
@@ -23,13 +46,16 @@ const HomeScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
+    paddingVertical: 50,
     flex: 1,
     alignItems: "center",
-    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    justifyContent: "center",
+    textAlign: "center",
   },
   imageContainer: {
     width: "90%",
-    height: "60%",
+    height: "40%",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -38,16 +64,23 @@ const styles = StyleSheet.create({
     height: "100%",
     resizeMode: "contain",
   },
+  textContainer: {
+    marginVertical: 30,
+  },
   boldText: {
+    color: "#7A1FA0",
     fontWeight: "bold",
     fontSize: 20,
     marginTop: 20,
+    textAlign: "center",
   },
   descriptionText: {
     fontSize: 16,
     marginTop: 10,
+    textAlign: "center",
   },
   buttonContainer: {
+    gap: 10,
     marginTop: 20,
     flexDirection: "column",
     justifyContent: "space-around",
